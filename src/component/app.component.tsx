e948c48df7de65094e8f9f879232876af5949698
Tangent Chang <andy23512@gmail.com>
2025-11-10 20:08:56 +0800

    fix: fix highlight bug of space key

--- src/component/app.component.tsx
+++ src/component/app.component.tsx
@@ -122,6 +122,7 @@ function AppComponent() {
   const selectedKeyboardLayout = KEYBOARD_LAYOUTS.find(
     (k) => k.id === selectedKeyboardLayoutId
   ) as KeyBoardLayout;
+  selectedKeyboardLayout.layout.Space = { unmodified: " " };
   const characterKeyCodeMap = convertKeyboardLayoutToCharacterKeyCodeMap(
     selectedKeyboardLayout
   );
