b3ab2d3148e44a2132ce13deca294da7ac2e06d1
Tangent Chang <andy23512@gmail.com>
2025-11-14 21:50:55 +0800

    feat: add opacity control and info button

--- src/component/app.component.tsx
+++ src/component/app.component.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useRef } from "react";
+import { Popover } from "@mui/material";
+import classNames from "classnames";
+import { MouseEvent, useEffect, useRef, useState, WheelEvent } from "react";
 import Moveable from "react-moveable";
 import "./app.component.css";
 import LayoutContainerComponent from "./layout-container.component";
@@ -6,6 +8,11 @@ import LayoutContainerComponent from "./layout-container.component";
 function AppComponent() {
   const mainDivRef = useRef(null);
   const moveableRef = useRef<Moveable>(null);
+
+  const [editMode, setEditMode] = useState(false);
+  const [infoPopoverAnchor, setInfoPopoverAnchor] =
+    useState<HTMLButtonElement | null>(null);
+  const [opacity, setOpacity] = useState<number>(1);
 
   useEffect(() => {
     const handleWindowResize = () => {
@@ -18,18 +25,86 @@ function AppComponent() {
     };
   }, []);
 
+  const handleSettingButtonClick = () => {
+    setEditMode((prevEditMode) => !prevEditMode);
+  };
+
+  const handleInfoButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setInfoPopoverAnchor((prev) => (!!prev ? null : event.currentTarget));
+  };
+
+  const handleInfoPopoverClose = () => {
+    setInfoPopoverAnchor(null);
+  };
+
+  const handleWheel = (event: WheelEvent<HTMLDivElement>) => {
+    if (!editMode) {
+      return;
+    }
+    event.preventDefault();
+    if (event.deltaY === 0) {
+      return;
+    }
+    const delta = event.deltaY < 0 ? 0.1 : -0.1;
+    let nextOpacity = opacity + delta;
+    if (nextOpacity > 1) {
+      nextOpacity = 1;
+    }
+    if (nextOpacity < 0.2) {
+      nextOpacity = 0.2;
+    }
+    setOpacity(nextOpacity);
+  };
+
+  const infoPopoverOpen = Boolean(infoPopoverAnchor);
+
   return (
     <>
       <div
         ref={mainDivRef}
         className="absolute bottom-0 left-1/2 h-68"
-        style={{ transform: "translateX(-50%)" }}
+        style={{ transform: "translateX(-50%)", opacity }}
+        onWheel={handleWheel}
       >
         <LayoutContainerComponent />
+        <div className="absolute right-2 bottom-2 flex flex-col gap-1">
+          {editMode && (
+            <>
+              <button
+                className="material-icons"
+                onClick={handleInfoButtonClick}
+              >
+                info
+              </button>
+            </>
+          )}
+          <button
+            className={classNames("material-icons", {
+              active: editMode,
+            })}
+            onClick={handleSettingButtonClick}
+          >
+            settings
+          </button>
+          <Popover
+            open={infoPopoverOpen}
+            anchorEl={infoPopoverAnchor}
+            onClose={handleInfoPopoverClose}
+            anchorOrigin={{ vertical: "center", horizontal: "right" }}
+            transformOrigin={{ vertical: "center", horizontal: "left" }}
+            disableScrollLock
+          >
+            <div className="overflow-hidden p-1">
+              Drag to move. <br />
+              Drag the control points to resize. <br />
+              Scroll up or down to adjust the transparency.
+            </div>
+          </Popover>
+        </div>
       </div>
       <Moveable
         ref={moveableRef}
-        target={mainDivRef}
+        target={editMode ? mainDivRef : null}
         draggable={true}
         onDrag={(e) => {
           e.target.style.transform = e.transform;
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -7,7 +7,7 @@ appContainer.id = "monkeytype-cc-extension-root";
 appContainer.style.position = "fixed";
 appContainer.style.width = "100%";
 appContainer.style.height = "100vh";
-appContainer.style.zIndex = "1";
+appContainer.style.zIndex = "1000";
 appContainer.style.top = "0px";
 document.body.appendChild(appContainer);
 
