a774ac018f82158d4decbadd09a4d879152b4227
Tangent Chang <andy23512@gmail.com>
2025-11-14 08:24:25 +0800

    feat: add snappable feature

--- src/component/app.component.tsx
+++ src/component/app.component.tsx
@@ -22,11 +22,23 @@ function AppComponent() {
           e.target.style.transform = e.transform;
         }}
         resizable={true}
-        keepRatio={true}
+        keepRatio={false}
         onResize={(e) => {
           e.target.style.width = `${e.width}px`;
           e.target.style.height = `${e.height}px`;
           e.target.style.transform = e.transform;
+        }}
+        snappable={true}
+        bounds={{ left: 0, top: 0, right: 0, bottom: 0, position: "css" }}
+        verticalGuidelines={[document.body.clientWidth / 2]}
+        horizontalGuidelines={[document.body.clientHeight / 2]}
+        snapDirections={{
+          top: true,
+          left: true,
+          center: true,
+          right: true,
+          bottom: true,
+          middle: true,
         }}
       ></Moveable>
     </>
